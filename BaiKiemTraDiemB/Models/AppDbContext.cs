db4d2b9e72b6095df2e76c0856c1a80d893a2165
lethanhtamm <lethanhtam230503@gmail.com>
2025-05-26 15:54:40 +0700

    commit

--- BaiKiemTraDiemB/Models/AppDbContext.cs
+++ BaiKiemTraDiemB/Models/AppDbContext.cs
@@ -5,6 +5,7 @@ namespace BaiKiemTraDiemB;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppContext>options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppContext> options) : base(options) { }
     public DbSet<DemoABC> DemoABC { get; set; }
 }
+
