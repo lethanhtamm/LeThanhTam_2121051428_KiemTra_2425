716bf626fc620c06d08fc70640437797b0729fbf
lethanhtamm <lethanhtam230503@gmail.com>
2025-06-04 09:35:58 +0700

    Commit

---
+++ BaiKiemTraDiemB/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using BaiKiemTraDiemB.Models;
+
+namespace BaiKiemTraDiemB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+        public DbSet <DemoABC> DemoABCs { get; set; }
+    }
+}
